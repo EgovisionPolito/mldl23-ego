3aa370a2964b1cb673ddbf71a5519306dd3fb2e5
AlbertoForesti <alberto.foresti.01@icloud.com>
2023-04-29 16:35:45 +0200

    trying to change len(record)

--- utils/loaders.py
+++ utils/loaders.py
@@ -76,7 +76,7 @@ class EpicKitchensDataset(data.Dataset, ABC):
         #           num_clip x num_frames_per_clip                       #
         ##################################################################
          
-        tot_frames=len(record)
+        tot_frames=record.num_frames()
         n_centroids=self.num_clips
         n_frames=self.num_frames_per_clip
         
@@ -100,7 +100,7 @@ class EpicKitchensDataset(data.Dataset, ABC):
         #           num_clip x num_frames_per_clip                       #
         ##################################################################
          
-        tot_frames=len(record)
+        tot_frames=record.num_frames()
         n_centroids=self.num_clips
         n_frames=self.num_frames_per_clip
         
