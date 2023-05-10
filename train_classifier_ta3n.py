d981f81b9f9039fdd69294a9efff825e4cc44c27
FfrancescoBorgna <95613733+FfrancescoBorgna@users.noreply.github.com>
2023-05-10 15:48:36 +0200

    validate correction

--- train_classifier_ta3n.py
+++ train_classifier_ta3n.py
@@ -231,22 +231,7 @@ def validate(model, val_loader, device, it, num_classes):
         for i_val, (data, label) in enumerate(val_loader):
             label = label.to(device)
 
-            for m in modalities:
-                batch = data[m].shape[0]
-                logits[m] = torch.zeros((args.test.num_clips, batch, num_classes)).to(device)
-
-            clip = {}
-            for i_c in range(args.test.num_clips):
-                for m in modalities:
-                    clip[m] = data[m][:, i_c].to(device)
-
-                output, _ = model(clip)
-                for m in modalities:
-                    logits[m][i_c] = output[m]
-
-            for m in modalities:
-                logits[m] = torch.mean(logits[m], dim=0)
-
+            logits = model(data)
             model.compute_accuracy(logits, label)
 
             if (i_val + 1) % (len(val_loader) // 5) == 0:
